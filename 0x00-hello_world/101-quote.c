010ee3712930cbd133d9221535a3230f340323e5
Juliet-weyula <J.weyula@gmail.com>
2022-12-08 14:11:41 -0800

    Unix quotes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,25 +1,18 @@
 #include <stdio.h>
 
-
-
 /**
  *
  *  * main - Entry point
  *
- *   *
+ *   *Description: Unix quotes
  *
  *    * Return: Always 0 (Success)
- *
  *     */
 
 int main(void)
-
 {
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
 
-		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
-
-
-
-			return (1);
-
+	return (1);
 }
+
